0651a49b15562d0033152b564db0ea45b9cd234d
Abdullah Bin Tahir <abdullahbintahir42@gmail.com>
2025-09-09 05:24:49 -0700

    task 3

--- round2/task3/main.py
+++ round2/task3/main.py
@@ -1,4 +1,12 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.kera
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, concatenate
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import os
+import matplotlib.pyplot as plt
